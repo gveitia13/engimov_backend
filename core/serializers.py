564c824a0342165305b9e2d42b74b62ac946759b
_BL.Init10 <blinit10@gmail.com>
2023-07-10 13:38:54 -0400

    Add depth meta attr to core/serializers.py

--- core/serializers.py
+++ core/serializers.py
@@ -15,13 +15,18 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('sku', 'category', 'name', 'description', 'image', 'price', 'visible')
-
-class WorkSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Work
-        fields = ('id', 'category', 'name', 'description', 'image')
+        depth = 1
 
 class TestimonialSerializer(serializers.ModelSerializer):
     class Meta:
         model = Testimonial
         fields = ('id', 'work', 'name', 'link', 'testimonial')
+
+class WorkSerializer(serializers.ModelSerializer):
+    testimonials = TestimonialSerializer(many=True, read_only=True, source='testimonial_set')
+    class Meta:
+        model = Work
+        fields = ('id', 'category', 'name', 'description', 'image', 'testimonials')
+        depth = 1
+
+
