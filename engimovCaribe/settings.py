47a9502748e29e18ed15bb51426c1b16cda7e77b
_BL.Init10 <blinit10@gmail.com>
2023-07-10 21:51:33 -0400

    Add depth meta attr to core/serializers.py

--- engimovCaribe/settings.py
+++ engimovCaribe/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = ['https://*.127.0.0.1',]
+CSRF_TRUSTED_ORIGINS = ['https://*.127.0.0.1', 'https://*.render.com', 'https://*.onrender.com']
 
 # Application definition
 
