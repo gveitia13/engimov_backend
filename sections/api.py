0e2c0d20d9ea4443ae10fa6e62d35f73564d582b
_BL.Init10 <blinit10@gmail.com>
2023-07-12 00:18:18 -0400

    Remove Auth logic from API Backend

--- sections/api.py
+++ sections/api.py
@@ -6,36 +6,36 @@ from .serializers import IndexSectionSerializer, AboutUsSectionSerializer, Produ
     SellProductsSectionSerializer
 
 
-class IndexSectionViewSet(viewsets.ModelViewSet):
+class IndexSectionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = IndexSection.objects.order_by('-id')
     serializer_class = IndexSectionSerializer
 
 
-class AboutUsSectionViewSet(viewsets.ModelViewSet):
+class AboutUsSectionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = AboutUsSection.objects.order_by('-id')
     serializer_class = AboutUsSectionSerializer
 
 
-class ProductsPortfolioSectionViewSet(viewsets.ModelViewSet):
+class ProductsPortfolioSectionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ProductsPortfolioSection.objects.order_by('-id')
     serializer_class = ProductsPortfolioSectionSerializer
 
 
-class WorksPortfolioSectionViewSet(viewsets.ModelViewSet):
+class WorksPortfolioSectionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = WorksPortfolioSection.objects.order_by('-id')
     serializer_class = WorksPortfolioSectionSerializer
 
 
-class WorkWithUsSectionViewSet(viewsets.ModelViewSet):
+class WorkWithUsSectionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = WorkWithUsSection.objects.order_by('-id')
     serializer_class = WorkWithUsSectionSerializer
 
 
-class ContactUsSectionViewSet(viewsets.ModelViewSet):
+class ContactUsSectionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ContactUsSection.objects.order_by('-id')
     serializer_class = ContactUsSectionSerializer
 
 
-class SellProductsSectionViewSet(viewsets.ModelViewSet):
+class SellProductsSectionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = SellProductsSection.objects.order_by('-id')
     serializer_class = SellProductsSectionSerializer
